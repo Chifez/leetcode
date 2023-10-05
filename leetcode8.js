72fb539c8c9dbd448b94f8d627e731e7febaddf8
emmanuel <chifez1@gmail.com>
2023-10-05 22:35:22 +0100

    leetcode7-alternative-solution:first letter to appear twice

--- leetcode8.js
+++ leetcode8.js
@@ -1,12 +1,23 @@
 //first letter to appear twice
 
 //bruteforce
-const repeatedCharacter = (s) => {
-  let newArray = [];
-  for (let i = 0; i < s.length; i++) {
-    if (newArray.includes(s[i])) return s[i];
-    newArray.push(s[i]);
-  }
-};
+// const repeatedCharacter = (s) => {
+//   let newArray = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if (newArray.includes(s[i])) return s[i];
+//     newArray.push(s[i]);
+//   }
+// };
 
-console.log(repeatedCharacter('aabcbcbaacz'));
+//reinforced
+const repeatedCharacter = (s) => {
+  let newObject = {};
+  for (let i of s) {
+    if (newObject[i] != undefined) {
+      return i;
+    }
+    newObject[i] = 1;
+  }
+  return '';
+};
+console.log(repeatedCharacter('abccbaacz'));
